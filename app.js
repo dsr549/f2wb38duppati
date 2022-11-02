cb1a9a6e620beb874a1a1dda252dd3ea453dc831
sudheer duppati <S555338@nwmissouri.edu>
2022-11-02 18:39:09 -0500

    added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var HorseRouter = require('./routes/Horse');
+var horseRouter = require('./routes/horse');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/Horse', HorseRouter);
+app.use('/horse', horseRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/Horse.js
+++ routes/Horse.js
@@ -1,13 +1,9 @@
 var express = require('express');
-
 var router = express.Router();
 
 /* GET home page. */
-
 router.get('/', function(req, res, next) {
-
-  res.render('Horse', { title: 'Search Results horse' });
-
+  res.render('horse', { title: 'Search Results Horse' });
 });
 
 module.exports = router;
